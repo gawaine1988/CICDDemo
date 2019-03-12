a0467908cd9ac151a93ecc023c7549b367f8d518
gawaine1988@163.com <gawaine1988@163.com>
2019-03-12 22:59:48 +0800

    add print ss

--- src/main/java/com/example/cidemo/CidemoApplication.java
+++ src/main/java/com/example/cidemo/CidemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CidemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("ss");
         SpringApplication.run(CidemoApplication.class, args);
     }
 
